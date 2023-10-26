6935d2e4e9b27b338ccbac8ddb722e6a0ffa0243
Bhushankumar Dattatray Khade <128994860+bhushankumar28@users.noreply.github.com>
2023-10-26 14:10:28 +0530

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import javax.swing.*;
+
+public class SnakeGame extends JFrame {
+
+    
+    public static void main(String[] args) {
+
+            new LoginFrame();
+        }
+    }
